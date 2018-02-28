f2e44768853f1c7760269451f8b43a93bb94eecf
ttwings <ttwings@qq.com>
2018-02-28 18:24:23 +0800

    角色图片以前忘记上传了

--- assets/tileMaps/cdda.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="3136" columns="16">
- <image source="../Graphics/Tilesets/tiles.png" width="512" height="6272"/>
-</tileset>
